b04df328972324e4b569d7c1b02b36890370041c
gaurav-gaikwad-12071998 <122888092+gaurav-gaikwad-12071998@users.noreply.github.com>
2026-04-02 12:28:51 +0530

    improved oss
    - update workflow files
    - update overview in readme.md

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -16,6 +16,7 @@ package main
 import (
 	"fmt"
 	"log"
+
 	"github.com/aptlogica/sereni-antivirus-clamav/internal/config"
 	"github.com/aptlogica/sereni-antivirus-clamav/internal/handlers"
 	"github.com/aptlogica/sereni-antivirus-clamav/internal/providers/antivirus"
