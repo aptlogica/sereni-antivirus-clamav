89f56e92853a7f15cf813f281197b068fea2f867
gaurav-gaikwad-12071998 <122888092+gaurav-gaikwad-12071998@users.noreply.github.com>
2026-04-19 10:29:10 +0530

    update licence

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,17 @@
 /*
 Copyright (c) 2026 Aptlogica Technologies Private Limited
 This file is part of software developed by Aptlogica Technologies Private Limited.
-Licensed under the MIT License. See the LICENSE file in the project root
-for full license information.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 Websites:
 https://www.aptlogica.com
 https://www.serenibase.com
@@ -25,7 +34,6 @@ import (
 
 	"github.com/aptlogica/sereni-antivirus-clamav/docs"
 )
-
 
 // @title Sereni Antivirus API
 // @version 1.0
